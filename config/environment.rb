555f3eb957317edf6308b3ab83c7f2e6fa22e74a
Madhu <email@example.com>
2019-10-25 01:24:48 +0100

    Creatd home page with AboutMe and added Posts

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+
+Rails.application.configure do
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+   address:              'smtp.gmail.com',
+   port:                 587,
+   domain:               'example.com',
+   user_name:            'sravikapaluru190719@gmail.com',
+   password:             '143@maanas',
+   authentication:       'plain',
+   enable_starttls_auto: true  
+}
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/contact', to:'contact#index'
+  root "pages#index"
+resources :posts
+  get '/about', to: 'pages#about'
+  
+
+ 
 end
