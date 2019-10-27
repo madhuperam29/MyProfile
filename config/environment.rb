5e36f4a0cc31809283cb792bed88e065ca1d84d7
Madhu <email@example.com>
2019-10-27 12:38:57 +0000

    Added Authentication and Admin User

--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,7 @@ Rails.application.initialize!
 
 Rails.application.configure do
 config.action_mailer.delivery_method = :smtp
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 config.action_mailer.smtp_settings = {
    address:              'smtp.gmail.com',
    port:                 587,
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
 	get '/contact', to:'contact#index'
 	root "pages#index"
 	resources :posts do
